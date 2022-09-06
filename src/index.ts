9fbcc7c7c0875bf50fb845db3480fb7dca0752df
Darren <darren.gannon@turnbuckle.co.za>
2022-09-06 06:25:43 +0200

    fix: npm build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './auth0-management.module'
+export * from './permission/permission'
+export * from './permission/permission-input'
+export * from './permission/permission.service'
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,37 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { ExecutionContext, INestApplication } from '@nestjs/common';
+import { Request } from 'express';
 import { AppModule } from './app/app.module';
-import axios from 'axios';
-import request from 'supertest';
+import { Auth0Module, AuthzGuard } from '../src';
+import { APP_GUARD } from '@nestjs/core';
+import { PermissionsGuard } from '../src/permissions/permissions.guard';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
 
-    const access_token = await axios.post('https://auth0-nestjs.us.auth0.com/oauth/token', {
-      "client_id": "rhSs1I3A5OMnGdTJ6yDVIyP0kggBgRRy",
-      "client_secret": "TylHZwU4NFmuBhgOXA5-FThHEopZvb73Gfj9wDe6-hajq2Yai9ozA26LQhM42bWe",
-      "audience": "https://auth0-nestjs.us.auth0.com/api/v2/",
-      "grant_type": "client_credentials"
-    }).then(resp => resp.data)
-      .then(data => data.access_token)
-
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         AppModule,
+        Auth0Module.register({
+          issuer: 'https://darren-anchorpoint.us.auth0.com/',
+          audience: 'http://localhost:3001',
+          extractUserPermissions: (context: ExecutionContext) => {
+            const req: Request = context.switchToHttp().getRequest()
+            return req.user.permissions;
+          },
+        }),
       ],
+      providers: [
+        {
+          provide: APP_GUARD,
+          useClass: AuthzGuard,
+        },
+      ]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   })
 
+  it('1 + 1 = 2', () => {
+    expect(1 + 1).toBe(2)
+  })
+
   afterAll(async () => {
     await app.close()
   })
 
-  it('1 + 1 = 2', () => {
-    expect(1 + 1).toBe(2)
+  it('should define permission gaurd', () => {
+    const gaurd = app.get(PermissionsGuard)
+
+    expect(gaurd).toBeDefined();
   })
 });
