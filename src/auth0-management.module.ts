21ab28349587d3501e16da7b908eb295a41d7183
Darren <darren.gannon@turnbuckle.co.za>
2022-09-06 07:04:41 +0200

    fix: add access token service as a provider

--- src/auth0-management.module.ts
+++ src/auth0-management.module.ts
@@ -3,10 +3,19 @@ import { HttpModule } from '@nestjs/axios'
 import { ConfigurableModuleClass } from './auth0.module-definition';
 import { UserService } from './user/user.service';
 import { PermissionService } from './permission/permission.service';
+import { Reflector } from '@nestjs/core';
+import { AccessTokenService } from './access-token/access-token.service';
 
 @Module({
-  imports: [HttpModule],
-  providers: [UserService, PermissionService],
+  imports: [
+    HttpModule
+  ],
+  providers: [
+    Reflector,
+    UserService, 
+    PermissionService,
+    AccessTokenService,
+  ],
   exports: [UserService],
 })
 export class Auth0ManagementModule extends ConfigurableModuleClass { }
